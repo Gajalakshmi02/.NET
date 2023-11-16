52722a5671423f707ca2bec71ea9ce031ad4428b
gajalakshmi22 <150905689+gajalakshmi22@users.noreply.github.com>
2023-11-16 12:59:57 +0530

    Add files via upload

---
+++ syntax and data type/syntax and data type/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+class DataTypes 
+{
+    static void Main()
+    {
+        int i = 24;
+        long myNum = 9000000000;
+        Console.WriteLine("Int: " + int.MinValue + ", " + int.MaxValue);
+        Console.WriteLine("my Long number is : " + myNum);
+
+        float f = 3.14f;
+        double d = 36.141592653;
+        decimal myDecimal = 123.456m;
+        Console.WriteLine("my Float value is : " + f);
+        Console.WriteLine("my Double num is : " + d);
+        Console.WriteLine("my Decimal point is : " + myDecimal);
+
+        char c = 'A';
+        string myName = "Hello, Gajalakshmi...!";
+        Console.WriteLine("Char: " + c);
+        Console.WriteLine("String: " + myName);
+
+        bool boo = true;
+        bool bo = false;
+        Console.WriteLine(boo);
+        Console.WriteLine(bo);
+    }
+}
