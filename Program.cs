e184f616c0846d219327d5fe3562a963f7ca078f
gajalakshmi22 <150905689+gajalakshmi22@users.noreply.github.com>
2023-11-16 12:47:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using class___object;
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        class___object.Student g = new class___object.Student();
+        g.name = "Gaja";
+        g.age = 22;
+        Console.WriteLine(g.name + " "  + g.age);
+    }
+}
+
+
+   
